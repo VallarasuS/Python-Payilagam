33d06474e79206ee47a41b709f351507aa663da5
Vallarasu Sambathkumar <vals@live.in>
2025-11-22 10:30:36 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+
+# print is a function, that writes given data to output device
+
+print("Hello there!")
+
+name = None
+age = None
+phone = None
+education = None
+
+name = input("enter your name: ")
+print("Hello " + name + ". Nice to meet you.")
+
+age = input("enter your age: ")
+print("Your age is: " + age )
+
+phone = input("enter your phone: ")
+print("Your phone number is: " + phone)
+
+education = input("enter your education: ")
+print("Your education number is: " + education)
