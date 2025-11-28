c22b2690503df79f3555c5776692ab4fdc31c659
Vallarasu Sambathkumar <vals@live.in>
2025-11-28 11:21:55 +0530

    loops

---
+++ functions.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    return x + y
+
+def incr(x):
+    return add(x, 1)
+
+incr(2)
---
+++ loops.py
@@ -0,0 +1,16 @@
+seconds = 1
+
+while seconds <= 60:
+    print(seconds)
+    seconds = seconds + 1
+
+while True:
+    print(seconds)
+    seconds = seconds + 1
+
+    if seconds > 60:
+        break
+
+
+def print_update(number):
+    pass
