6459e56fb4b44d6013e316e64f1566c9ca6e978c
Vallarasu Sambathkumar <vals@live.in>
2026-01-19 19:29:55 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,27 @@
+# this is function definition
+def hello():
+    print("Hello World")
+    print("From Python")
+
+
+# function call
+# hello()
+
+
+# function with a parameter / argument
+def say_hello(name):
+    print("Hello", name)
+
+
+say_hello("John")
+
+print("End of program")
+
+
+def add(x, y):
+    print(x + y)
+
+
+add(10, 3)
+add(10, 20)
+add(10, 5)
