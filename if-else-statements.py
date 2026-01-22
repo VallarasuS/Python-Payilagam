14dd841be24bbf83adc873c26bdf0d695c49e3f8
Vallarasu Sambathkumar <vals@live.in>
2026-01-22 18:28:01 +0530

    if elif else

---
+++ if-else-statements.py
@@ -0,0 +1,29 @@
+# x = 30
+
+# if x > 20:
+#     print("X is greater than 20")
+
+
+y = 9
+
+if y < 10:
+    print("Single digit input")
+    print("Single digit input")
+else:
+    print("More than single digit")
+    print("More than single digit")
+
+
+day = 2
+
+if day == 1:
+    print("mon")
+elif day == 2:
+    print("tue")
+elif day == 3:
+    print("wed")
+else:
+    print("invalid input")
+
+
+print("End of Program")
--- logical-op.py
+++ logical-op.py
@@ -1,35 +1,59 @@
-age = int(input("Enter your age: "))
-voter_id_input = input("Do you have voter id? ")
+# age = int(input("Enter your age: "))
+# voter_id_input = input("Do you have voter id? ")
 
-has_voter_id = voter_id_input == "yes"
-can_vote = age >= 18 and has_voter_id
+# has_voter_id = voter_id_input == "yes"
+# can_vote = age >= 18 and has_voter_id
 
-print("Can Vote ? ", can_vote)
+# print("Can Vote ? ", can_vote)
 
-# Truthy VAlue
-
-
-age = 34
-accompanied_by_adult = True
+# # Truthy VAlue
 
 
-def is_parent_present():
-    print("Hello")
-    return accompanied_by_adult
+# age = 34
+# accompanied_by_adult = True
 
 
-# short circute
-allow_entry = age >= 18 or is_parent_present()
-
-deny_entry = not allow_entry
-
-
-print("Allow Entry ", allow_entry)
+# def is_parent_present():
+#     print("Hello")
+#     return accompanied_by_adult
 
 
-# children with access to tab
-# allow access to specific sites only
-# white_list -> .edu, .wiki, .org
+# # short circute
+# allow_entry = age >= 18 or is_parent_present()
 
-# deny access to certain sites
-# black_listed -> social, drugs, violence
+# deny_entry = not allow_entry
+
+
+# print("Allow Entry ", allow_entry)
+
+
+# # children with access to tab
+# # allow access to specific sites only
+# # white_list -> .edu, .wiki, .org
+
+# # deny access to certain sites
+# # black_listed -> social, drugs, violence
+
+
+# def is_primary(color):
+#     up_color = color.upper()
+#     result = up_color == "RED" or up_color == "GREEN" or up_color == "BLUE"
+#     print("Is Primary Color :", color, result)
+
+
+# is_primary("Red")  # expected to be True
+# is_primary("green")  # expected to be True
+# is_primary("yellow")
+
+
+def is_week_end(day):
+    day = day.lower()
+    print("Is week end? ", day == "sunday" or day == "saturday")
+
+
+is_week_end("SaturDay")
+is_week_end("sunDay")
+is_week_end("Monday")
+
+# change to all upper case, or all lower case
+# RED, red
