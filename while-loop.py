2e6ad928e280fa73465b4f07b6ba8e65d8b97bf4
Vallarasu Sambathkumar <vals@live.in>
2026-01-27 11:17:47 +0530

    Create while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,32 @@
+# while <expression>:
+#     <loop body>
+
+# <out side of loop>
+
+# n = 1
+
+# while n <= 10:
+#     print(n)
+#     n = n + 1
+
+
+# n = 1  # start
+
+# while n <= 100:  # stop
+#     print(n)
+#     n = n + 3  # step
+
+
+greet = "Hello World"
+print(greet)
+print(len(greet))
+print(greet[4])
+
+n = 0
+
+while n < len(greet):
+    print(greet[n])
+    n = n + 1
+
+
+print("End of Program")
