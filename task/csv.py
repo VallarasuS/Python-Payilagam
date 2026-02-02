a0ed788cea3206316ec25a41be690ce4b74f0612
Vallarasu Sambathkumar <vals@live.in>
2026-02-02 17:26:11 +0530

    Update csv.py

--- task/csv.py
+++ task/csv.py
@@ -11,3 +11,13 @@
 # Steve, Bangalore, 34, 60, 50
 
 data = "Name, City, Math, Science, Language \n John, Chennai, 40, 60, 50 \n Dave, Chennai, 34, 60, 50 \n Steve, Bangalore, 34, 60, 50"
+
+# string
+# - split \n
+#   ignore first line
+# - split by , -> 5 tokens or words
+# process numerics, POS 2,3,4 []
+# calculate sum, average, top
+# add it back to the line
+# using join / concatenation
+# combine all lines
