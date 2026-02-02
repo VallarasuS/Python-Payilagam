2bcaa88709e3ddb5a4b76f79754a112f809bd0a3
Vallarasu Sambathkumar <vals@live.in>
2026-02-02 17:18:09 +0530

    Create csv.py

---
+++ task/csv.py
@@ -0,0 +1,13 @@
+# Input
+# Name, City, Math, Science, Language
+# John, Chennai, 40, 60, 50
+# Dave, Chennai, 34, 60, 50
+# Steve, Bangalore, 34, 60, 50
+
+# Expected Output
+# Name, City, Math, Science, Language, Total, Average, Top Score
+# John, Chennai, 40, 60, 50, 150, 50, 60
+# Dave, Chennai, 34, 60, 50
+# Steve, Bangalore, 34, 60, 50
+
+data = "Name, City, Math, Science, Language \n John, Chennai, 40, 60, 50 \n Dave, Chennai, 34, 60, 50 \n Steve, Bangalore, 34, 60, 50"
