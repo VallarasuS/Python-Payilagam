df9cf35b5bcd1f6bcbf35171cdeb55260460064f
Vallarasu Sambathkumar <vals@live.in>
2026-02-04 17:42:19 +0530

    Update csv.py

--- task/csv.py
+++ task/csv.py
@@ -21,3 +21,38 @@ data = "Name, City, Math, Science, Language \n John, Chennai, 40, 60, 50 \n Dave
 # add it back to the line
 # using join / concatenation
 # combine all lines
+
+# Built-in fns:
+# sum
+# len
+# max
+# min
+# type
+
+
+def split_lines(source):
+    lines = source.split("\n")
+    return lines[1 : len(lines) : 1]
+
+
+def split_tokens(source):
+    words = source.split(",")
+    return words[2 : len(words) : 1]
+
+
+lines = split_lines(data)
+
+for line in lines:
+    tokens = split_tokens(line)
+    print(tokens)
+
+    total = 0
+    top = 0
+
+    for word in tokens:
+        total = total + int(word)
+        top = max(top, int(word))
+
+    print(total)
+    print(total / len(tokens))
+    print(top)
