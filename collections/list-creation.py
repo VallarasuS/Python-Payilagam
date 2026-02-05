83d8d476b6e6a66087f59ce08b3658490c54c5fc
Vallarasu Sambathkumar <vals@live.in>
2026-02-05 17:02:41 +0530

    Create list-creation.py

---
+++ collections/list-creation.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+
+for i in numbers:
+    print(i)
+
+total = sum(numbers)
+print(total)
+
+top = max(numbers)
+print(top)
+
+low = min(numbers)
+print(low)
+
+text = list("Hello")
+print(text)
+
+print(dir(text))
