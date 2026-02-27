c69bee7c3345e57b779b19293f688c1be5c97f7c
Vallarasu Sambathkumar <vals@live.in>
2026-02-27 18:11:18 +0530

    Create tk-gui.py

---
+++ tk-gui.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+root.title = "Hello"
+
+label = Label(root, text="Hello World")
+label.pack()
+
+root.mainloop()
+
+
+# def add (x, y):
+#     return x + y
+
+
+# add(10, 20)
+# add(x= 10, y=20)
