72d3e966e1850eec171cdc50f9deac7fc5babfe8
Vallarasu Sambathkumar <vals@live.in>
2026-02-28 13:34:03 +0530

    hello world

---
+++ hello.py
@@ -0,0 +1,48 @@
+# This is a comment
+# Explanation / further details of code for humans
+# Ctrl + /
+
+# View -> Terminal
+# Ctrl + `
+
+print("Hello World")
+
+# To Run Python File
+# ###############################
+
+# python3 hello.py
+# python hello.py
+# py hello.py
+
+# Shortcut
+#################################
+# Ctrl + Shift + P
+# Python: Run Python File in Terminal
+# Ctrl + Enter
+
+
+# <variable-name> = <values>
+# = <- assignment operator
+
+name = "John"
+age = 30
+weight = 60.4
+married = True
+
+# prints value to console / terminal
+print(name)
+print(age)
+
+# read from console / terminal
+# name = input("Enter your name: ")
+# print(name)
+
+# education = input("Enter you degree: ")
+# print(education)
+
+x = input("Enter a number")
+y = input("Enter a number")
+x = int(x)  # str -> int
+y = int(y)  # str -> int
+z = x + y
+print(z)
