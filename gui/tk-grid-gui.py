c5258236d0e266645c58342be544538af5789373
Vallarasu Sambathkumar <vals@live.in>
2026-03-04 17:38:41 +0530

    Update tk-grid-gui.py

--- gui/tk-grid-gui.py
+++ gui/tk-grid-gui.py
@@ -14,10 +14,14 @@ root.geometry("800x500")
 # expand, fill, fit available space
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
 
 # layout container - panel, canvas, shell
-frame = ttk.Frame(root)
-frame.grid()
+frame_left = ttk.Frame(root)
+frame_left.grid(row=0, column=0)
+
+frame_right = ttk.Frame(root)
+frame_right.grid(row=0, column=1)
 
 # Grid Layout 3x2
 # -----------------
@@ -33,26 +37,30 @@ def on_add_clicked():
     print(name_entry.get())
     print(phone_entry.get())
 
+    data = name_entry.get() + ", " + phone_entry.get()
+    contact = ttk.Label(frame_right, text=data)
+    contact.pack()
+
 
 # Name - Row 0
 
-name = ttk.Label(frame, text="Name")
+name = ttk.Label(frame_left, text="Name")
 name.grid(row=0, column=0, padx=10, pady=10)
 
-name_entry = ttk.Entry(frame)
+name_entry = ttk.Entry(frame_left)
 name_entry.grid(row=0, column=1, padx=10, pady=10)
 
 # Phone - Row 1
 
-phone = ttk.Label(frame, text="Phone")
+phone = ttk.Label(frame_left, text="Phone")
 phone.grid(row=1, column=0, padx=10, pady=10)
 
-phone_entry = ttk.Entry(frame)
+phone_entry = ttk.Entry(frame_left)
 phone_entry.grid(row=1, column=1, padx=10, pady=10)
 
 # Add Button - Row 2
 
-add_button = ttk.Button(frame, text="Add", command=on_add_clicked)
+add_button = ttk.Button(frame_left, text="Add", command=on_add_clicked)
 add_button.grid(row=2, column=1, padx=10, pady=10, sticky="e")
 
 root.mainloop()
