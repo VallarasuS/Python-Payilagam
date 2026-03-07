4fed9f554a3111ff78da3a59f7a9c011224e4311
Vallarasu Sambathkumar <vals@live.in>
2026-03-07 18:06:57 +0530

    Create operators-arithmetic.py

---
+++ operators-arithmetic.py
@@ -0,0 +1,25 @@
+x = 10
+y = 3
+
+total = x + y  # 10 + 3 -> 13
+print("Total: ", total)
+
+difference = x - y  # 10 - 3 -> 7
+print("Difference: ", difference)
+
+product = x * y
+print("Product:", product)
+
+# True Division
+true_div = x / y  # 10 / 3 -> 3.3333
+print("True Div: ", true_div)
+
+# Floor Division    # 10 // 3 -> 3
+floor_div = x // y
+print("Floor Division", floor_div)
+
+exponent = x**y  # 10 ** 3 -> 1000
+print("Exponent:", exponent)
+
+remainder = x % 3  # 10 % 3 -> 1
+print("Remainder:", remainder)
