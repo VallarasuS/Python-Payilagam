4b88d01df2badebf8ddd9c630d9a115ea2495b0f
Vallarasu Sambathkumar <vals@live.in>
2026-03-22 10:22:54 +0530

    Loop

---
+++ if-else.py
@@ -0,0 +1,25 @@
+age = input("enter age")
+age = int(age)
+
+has_voter_id = False
+
+if age >= 18 and has_voter_id:
+    print("User can vote")
+else:
+    print("User can not vote")
+
+
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("Zero")
+elif num > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+
+# even number
+# odd number
+# divisible by 3
+# divisible by 3 and 5
---
+++ loop.py
@@ -0,0 +1,78 @@
+# LOOP
+
+# - for
+# - while
+
+# While
+# -------------------
+# Condition based
+# waiting for file download
+# waiting for bus arrival
+# waiting for tank to fill
+
+# For
+# -------------------
+# number of steps / iteration are know ahead of time
+# distributing 10 things
+# traveling 10 km
+# walking 100 steps
+
+# Print 10 number
+
+# 1, 2, 3, 4, .... 10
+
+# start - 1
+# stop - 10
+# step - 1
+
+# indefinite loop, runs for ever
+
+# 1, 2, 3 ... 10
+
+# x = 1
+# while x <= 10:
+#     print(x)
+#     x = x + 1
+
+# print("EOP")
+
+# 1,2,3.. 100
+# x = 1
+# while x <= 100:
+#     print(x)
+#     x = x + 1
+
+
+# 11 - 20
+
+# x = 11
+# while x <= 20:
+#     print(x)
+#     x = x + 1
+
+# 2, 4, 6, ... 20
+# start - 2
+# stop - 20
+# step - 2
+
+# x = 2
+# while x <= 20:
+#     print(x)
+#     x = x + 2
+
+# 5, 10, 15 ... 50
+# x = 5
+# while x <= 50:
+#     print(x)
+#     x = x + 5
+
+
+# x = 1
+# while x <= 10:
+#     print(x * 2)
+#     x = x + 1
+
+# x = 1
+# while x <= 10:
+#     print(x * 5)
+#     x = x + 1
