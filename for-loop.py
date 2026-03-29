5af6685d1ec58af82ca75b4caf3a8c7e3743d72e
Vallarasu Sambathkumar <vals@live.in>
2026-03-29 10:27:13 +0530

    loop

---
+++ for-loop.py
@@ -0,0 +1,88 @@
+# FOR LOOP
+#######################
+
+# No times known ahead
+
+# start, stop, step
+
+# x = 1
+# while x < 10:
+#     print(x * 2)
+#     x = x + 1
+
+# for var in seq:
+#     <body>
+
+# for x in range(1, 6, 1):
+#     print(x)
+
+# for x in range(2, 12, 2):
+#     print(x)
+
+# for x in range(5, 55, 5):
+#     print(x)
+
+
+# range
+# built in function (start, stop, step)
+
+# total
+# max
+# min
+
+# Reduce / Reduction
+# multiple input to single output
+
+# total = 0
+# for i in range(1, 11, 1):
+#     total = total + i
+
+# print(total)
+
+# fact = 1
+# for i in range(1, 11, 1):
+#     fact = fact * i
+
+# print(fact)
+
+# Map
+# for each input a matching output
+
+# for i in range(1, 11, 1):
+#     print(i * 5)
+
+# for i in range(1, 11, 1):
+#     print(i * 2)
+
+
+# message = "Hello"
+
+# for i in message:
+#     print(i)
+
+# find
+
+for i in range(1, 11, 1):
+    if i == 5:
+        print(i)
+
+# filter
+for i in range(1, 11, 1):
+    if i >= 5:
+        print(i)
+
+
+# def range(start, stop, step):
+
+#     bucket = []
+
+#     x = start
+#     while x < stop:
+#         bucket.append(x)
+#         x = x + step
+
+#     return bucket
+
+
+# seq = range(1, 101, 1)
+# print(seq)
--- loop.py
+++ loop.py
@@ -73,6 +73,15 @@
 #     x = x + 1
 
 # x = 1
-# while x <= 10:
+# while True:
 #     print(x * 5)
 #     x = x + 1
+
+
+# while gps != destination:
+#     lat_lan = get_gps()
+#     ping(lat_lan) # ping to server
+#     sleep(5)
+
+
+# Termination
