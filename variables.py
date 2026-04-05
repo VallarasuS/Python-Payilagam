e9fd60dabcc279f631009e30a1bac653a616dffe
Vallarasu Sambathkumar <vals@live.in>
2026-04-05 12:06:18 +0530

    Initial Commit

---
+++ variables.py
@@ -0,0 +1,63 @@
+# This is a comment
+
+# Ctrl + /
+# This is a comment
+
+# variable naming convention
+# lower case
+# no space
+# start with character
+# under_score allowed
+# no other special chars
+# avoid keywords in python
+
+# name = "John"
+# print(name)
+# print(type(name))
+
+# age = 30
+# print(age)
+
+# weight = 45.6
+# print(weight)
+
+# employed = True
+# print(employed)
+
+# age = 31
+# print(age)
+
+# age = "John"
+# print(age)
+
+
+# Type
+# str
+# int
+# float
+# bool
+
+# print()
+# type()
+
+# Arithmetic Operators
+
+# + - * / // % **
+
+x = 10
+y = 3
+
+print(x, y)
+
+
+z = x / y
+print(z)
+
+z = x // y
+print(z)
+
+result = x % y
+print(result)
+
+result = x ** y # 10 ** 3 -> 10 * 10 * 10
+print(result)
