5aa3794a8bbfdf8a101d9d87a4a0c4c68b51295d
Vallarasu Sambathkumar <vals@live.in>
2026-04-18 10:34:45 +0530

    Create fn_call.py

---
+++ fn_call.py
@@ -0,0 +1,15 @@
+
+def add(x, y):
+    return x + y
+
+result = add(10, 20)
+print(result)
+
+a = input("Enter numbers: ")
+a = int(a)
+
+b = input("Enter numbers: ")
+b = int(b)
+
+result = add(a, b)
+print(result)
