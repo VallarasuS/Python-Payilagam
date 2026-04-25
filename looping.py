422605aeac29d780a809bb849bfb6678b0efd4a4
Vallarasu Sambathkumar <vals@live.in>
2026-04-25 11:36:30 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,42 @@
+import time
+
+# print 1-10
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+# print 2, 4, 6 ... 20
+
+# i = 2
+# while i <= 20:
+#     print(i)
+#     i = i + 2
+
+# i = 5
+# while i <= 50:
+#     print(i)
+#     i = i + 5
+
+# print even numbers upto 100
+
+# i = 1
+# while i <= 100:
+#     if i % 5 == 0:
+#         print(i)
+#     i = i + 1
+
+# sum of 1 ... 10
+
+
+i = 1
+total = 0
+while i <= 100:
+    total = total + i
+    i = i + 1
+
+print(total)
+
+print("End")
+
