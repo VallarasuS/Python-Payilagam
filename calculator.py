2f36b6ee9bb1f769e1b1a7985a567dc0c57aeba4
Vallarasu Sambathkumar <vals@live.in>
2026-05-01 12:06:48 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,39 @@
+# calculator
+# print all options
+#   add
+#   mul
+#   div
+#   sub
+#   exit
+
+# get options from user
+# get numbers from user
+# perform operation
+# print results
+# repeat
+
+while True:
+
+    print("Calculator")
+    print("----------")
+    print(" add \n mul \n div \n sub \n exit")
+    option = input("Select an option form above: ")
+    option = option.lower()
+
+    num1 = int(input("Enter number one"))
+    num2 = int(input("Enter number two"))
+    result = None
+
+    if option == "add":
+        result = num1 + num2
+        print(result)
+    elif option == "mul":
+        pass
+    elif option == "div":
+        pass
+    elif option == "sub":
+        pass
+    elif option == "exit":
+        break
+    else:
+        print("Invalid Input, try again")
