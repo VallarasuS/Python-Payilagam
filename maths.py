ff5f93511819015fe2f551cea0fcb5147876fb84
Vallarasu Sambathkumar <vals@live.in>
2026-05-03 09:43:42 +0530

    packages

---
+++ maths.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+def power(x, y):
+    return x ** y
+
+def mul(x, y):
+    return x * y
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    sum = add(10, 20)
+    print(sum)
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-from tkinter import *
+# from tkinter import *
 
-window = Tk()
-window.title("Hello World")
-window.geometry("800x600")
+# window = Tk()
+# window.title("Hello World")
+# window.geometry("800x600")
 
-button = Button(window)
-button.configure(text="Click Me")
-button.pack()
+# button = Button(window)
+# button.configure(text="Click Me")
+# button.pack()
 
-window.mainloop()
+# window.mainloop()
+
+from maths import add, power
+
+sum = add(10, 20)
+print(sum)
+
+exp = power(10, 3)
+print(exp)
