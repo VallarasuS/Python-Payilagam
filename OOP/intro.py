2807e1733ce3502734c5d21e4564ae6ffe985c90
Vallarasu Sambathkumar <vals@live.in>
2026-05-08 12:10:55 +0530

    Create intro.py

---
+++ OOP/intro.py
@@ -0,0 +1,31 @@
+# Blue Print / Design
+class Calculator:
+    def add(self, x, y):
+        print(x + y)
+
+    def sub(x, y):
+        print(x - y)
+
+    def mul(x, y):
+        print(x * y)
+
+# Construction / Object
+calc = Calculator()
+calc.add(10, 20)
+
+calc_one = Calculator()
+calc_one.add(10, 40)
+
+
+# def add(x, y):
+#     print(x+y)
+
+# add(10, 20)
+
+class Student:
+
+    # dept
+    # year
+    # section
+    # name
+    # roll number
