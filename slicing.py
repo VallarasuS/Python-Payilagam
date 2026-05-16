081ccddea095736165c76a8e23c272cc9b8cb8c3
Vallarasu Sambathkumar <vals@live.in>
2026-05-16 20:23:06 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,23 @@
+# start : stop: step
+
+data = "all operations will return a copy of the string"
+print(data)
+
+words = data.split()
+print(words)
+
+words = words[start:stop:1]
+print(words)
+
+# x = data[::-1]
+# print(x)
+
+# x = data[4:len(data):1]
+# print(x)
+
+# x = data[4::]
+# print(x)
+
+# x = data[4]
+# print(x)
+
