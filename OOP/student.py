fdb14dfc237536e82e4ed2bb256a33c941cb1907
Vallarasu Sambathkumar <vals@live.in>
2026-05-22 11:48:14 +0530

    Create student.py

---
+++ OOP/student.py
@@ -0,0 +1,13 @@
+class Student:
+    
+    def __init__(self, name, id, marks):
+        pass
+
+        print(name)
+        print(id)
+        print(marks)
+
+sanjay = Student("sanjay", "123", [10, 20, 30, 40, 50])
+parithi = Student("parithi", "234", [20, 30, 40, 50, 60])
+
+
